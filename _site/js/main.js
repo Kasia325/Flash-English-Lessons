382affe86e2d67700378713abcf27f1aa73a9609
Kasia325 <32081621+Kasia325@users.noreply.github.com>
2017-09-24 11:16:30 +0800

    init

---
+++ _site/js/main.js
@@ -0,0 +1,23 @@
+// Load the DOM
+$(document).ready(function(){
+
+
+  //Log
+  console.log('Ready!');
+
+    // Click the hamburger icon
+    $('.toggle-mobile-menu').click(function(){
+
+        // Show the mobile menu
+        $('#mobile-menu').toggleClass('mobile-menu-visible');
+
+    });
+
+    // Show the mobile menu
+
+    // Click the cross item
+
+    // Close the mobile menu
+
+
+});
